7d1307d257f550201477a1dd65965749a05d66ac
Michael Egbinola <michaelmarketing25@gmail.com>
2023-03-15 16:17:24 -0400

    updated 01.usemodules.js

--- backend-1/modules/01.usemodules.js
+++ backend-1/modules/01.usemodules.js
@@ -1,4 +1,4 @@
-//using a module we created and exported in 02.utils.js
+//using a module we created and exported in 02.utils.js and 03.isSquaredutils.js
 
 const getName = require("./02.nameutils");
 const isSquared = require("./03.issquaredutils");
