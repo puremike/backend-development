30f4da40ae914b9189334e6f283aca1de65b2ce8
Michael Egbinola <michaelmarketing25@gmail.com>
2023-03-15 16:14:10 -0400

    Completed external modules

---
+++ backend-1/modules/01.usemodules.js
@@ -0,0 +1,9 @@
+//using a module we created and exported in 02.utils.js
+
+const getName = require("./02.nameutils");
+const isSquared = require("./03.issquaredutils");
+
+getName("Sandra", "Bullock");
+getName("Michael", "Egbinola");
+
+isSquared(81);
