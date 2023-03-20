d66a6d5e38da78e0a2453cc95d9968e3face666c
Michael Egbinola <michaelmarketing25@gmail.com>
2023-03-20 11:37:33 -0400

    updated modules name

--- 01backend-node/modules/01usemodules.js
+++ 01backend-node/modules/01usemodules.js
@@ -1,7 +1,7 @@
 //using a module we created and exported in 02.utils.js and 03.isSquaredutils.js
 
-const getName = require("./02.nameutils");
-const isSquared = require("./03.issquaredutils");
+const getName = require("./02nameutils");
+const isSquared = require("./03issquaredutils");
 const checkEvenOrOdd = require("./04checkevenodd");
 
 getName("Sandra", "Bullock");
